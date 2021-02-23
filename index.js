cba1d29d53f7a1e13e2dc7d79a0cf59745cb7545
Isiaka Abdulahi <isiakaabd@gmail.com>
2021-02-24 00:10:49 +0100

    catching error

--- index.js
+++ index.js
@@ -10,15 +10,21 @@ importJoke ()
 
  async function importJoke () {
 
-const config = {
-    headers: {
-    Accept: "application/json"
-    }
-}
+        const config = {
+            headers: {
+            Accept: "application/json"
+            }
+        }
+        try{
 
+            const res = await  fetch ("https://icanhazdadjoke.com", config)
+            const data = await  res.json()
+            jokes.innerHTML = data.joke
+        }
+        catch(err){
+           
+            jokes.innerHTML = `Failed to Load, Please Check Your connection.<br /> <br /><br /> REMLAD`
+          
+        }
 
-const res = await  fetch ("https://icanhazdadjoke.com", config)
-const data = await  res.json()
-
-jokes.innerHTML = data.joke
 }
